d3460d5ff27bbd3bf64984cac6e9acce159dbfa3
sangyup <ideallyup@naver.com>
2024-11-24 10:57:15 +0900

    edit db.js

--- docker_project/gcshop/lib/db.js
+++ docker_project/gcshop/lib/db.js
@@ -1,10 +1,10 @@
 var mysql = require("mysql2");
 
 var db = mysql.createConnection({
-  host: "127.0.0.1",
-  user: "root",
-  password: "root",
-  database: "webdb2024",
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
   multipleStatements: true,
 });
 
